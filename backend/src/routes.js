a04f4e76d3532a0f95a05c04c47e2de9596b0785
JulianoV <ventolajuliano@gmail.com>
2020-01-14 15:21:03 -0300

    Folder for Controllers and moved Dev post request

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,40 +1,9 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
 // Main route
-routes.post("/devs", async (req, res) => { 
-  // Get github user info from body on post route
-  const { github_username, techs, latitude, longitude } = req.body;
-
-  // Get response from github API with user information
-  const {data} = await axios.get(`https://api.github.com/users/${github_username}`);
-
-  // Get name (or login if name empty), avatar url and bio
-  const { name = login, avatar_url, bio } = data;
-
-  // Change each tech(text) in array as item
-  const techsArray = techs.split(",").map(tech => tech.trim());
-
-  // Create geolocation for lat & long (based on PointSchema)
-  const location = {
-    type: 'Point',
-    coordinates: [longitude, latitude],
-  }
-
-  // Create new user in Database
-  const dev = await Dev.create({
-    name,
-    github_username,
-    bio,
-    avatar_url,
-    techs: techsArray,
-    location
-  })
-
-  return res.json(dev);
-})
+routes.post("/devs",DevController.store);
 
 module.exports = routes;

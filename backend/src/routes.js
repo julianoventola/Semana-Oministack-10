399b6a5a4346a83cf4d5d6392b9d06a1122d93b4
JulianoV <ventolajuliano@gmail.com>
2020-01-14 20:03:59 -0300

    Update Dev function and route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,6 +11,9 @@ routes.get("/devs", DevController.index);
 // Post route to Add new Devs
 routes.post("/devs", DevController.store);
 
+// Put route to Edit Devs information
+routes.put("/devs/:github_username", DevController.update);
+
 // Search Dev by tech and geolocation
 routes.get("/search", SearchController.index);
 

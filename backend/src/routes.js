8eefa983663aaf5f966ef0b5d605e60f520ed4a4
JulianoV <ventolajuliano@gmail.com>
2020-01-14 15:00:39 -0300

    Create new dev in database using github api

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,33 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
 // Main route
-routes.post("/users", (req, res) => { 
-  const user = req.body
-  return res.json(user);
+routes.post("/devs", async (req, res) => { 
+  // Get github user info from body on post route
+  const { github_username, techs } = req.body;
+
+  // Get response from github API with user information
+  const {data} = await axios.get(`https://api.github.com/users/${github_username}`);
+
+  // Get name (or login if name empty), avatar url and bio
+  const { name = login, avatar_url, bio } = data;
+
+  // Change each tech(text) in array as item
+  const techsArray = techs.split(",").map(tech => tech.trim());
+
+  // Create new user in Database
+  const dev = await Dev.create({
+    name,
+    github_username,
+    bio,
+    avatar_url,
+    techs: techsArray
+  })
+
+  return res.json(dev);
 })
 
 module.exports = routes;

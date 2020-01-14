9c847762189233a0ba2280a0a30780199a6cfc92
JulianoV <ventolajuliano@gmail.com>
2020-01-14 14:17:23 -0300

    Add Mongoose for MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// Connect into MongoDb collection
+mongoose.connect(
+  'mongodb+srv://juliano10:juliano10@cluster0-ppp0b.mongodb.net/week10?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+// Enable server to get json information on routes
+app.use(express.json());
+
 // Main route
-app.get("/", (req, res) => {
-  return res.json({message: 'Hello all'});
+app.post("/users", (req, res) => { 
+  const user = req.body
+  return res.json(user);
 })
 
 app.listen(3333, () => console.log('Server running on port 3333...'));

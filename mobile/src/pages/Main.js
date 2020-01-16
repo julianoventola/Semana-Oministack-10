5eb4d4ecbfa9e025b115cd739ead488a9b8e5a61
JulianoV <ventolajuliano@gmail.com>
2020-01-16 19:54:49 -0300

    Load current region and Maps for user

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
-  return <View />
+
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(()=> {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const {latitude, longitude} = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.02,
+          longitudeDelta: 0.02,
+        });
+      }
+    }
+    loadInitialPosition();
+  },[])
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return <MapView initialRegion={currentRegion} style={styles.map}/>
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  }
+});
 
 export default Main;

9f57176a20f9478d05ba13d3bae86296fdde4f6f
JulianoV <ventolajuliano@gmail.com>
2020-01-16 19:20:43 -0300

    Basic style and default for Main and Profile

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "react-native";
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Universe</Text>
-    </View>
+    <>
+    <StatusBar barStyle="light-content" backgroundColor="#7D40E7"/>
+    <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
